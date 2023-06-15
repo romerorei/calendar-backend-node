c58f4bec4ddbd7ebd8d5b40a7adc5fc61567389f
Reinaldo Romero <reinaldo.romero@pjchile.com>
2023-06-14 23:07:31 -0400

    Add path *

--- index.js
+++ index.js
@@ -24,6 +24,10 @@ app.use( express.json() );
 app.use('/api/auth', require('./routes/auth') );
 app.use('/api/events', require('./routes/events') );
 
+app.get('*', ( req, res) => {
+  res.sendFile( __dirname + '/public/index.html' );
+})
+
 // Escchar peticiones
 app.listen( process.env.PORT , () => {
   console.log(`Servidor corriendo en puerto ${process.env.PORT}`);
